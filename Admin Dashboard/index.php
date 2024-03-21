816fc70c8a74cb30c8756ab1b7d032e335fffe1d
Ayyoub EL bouzekri idrissi <ayoovbelidrissi@gmail.com>
2024-03-21 11:15:38 +0000

    Style moddification

--- Admin Dashboard/index.php
+++ Admin Dashboard/index.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Responsive Admin Dashboard | Korsat X Parmaga</title>
-    <!-- ======= Styles ====== -->
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
