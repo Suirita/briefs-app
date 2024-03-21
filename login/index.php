b8448d5398d05f706fe34b50c9aecbeec3b3dd4a
Fahd <suirita.fahd.solicode.gmail.com>
2024-03-21 11:13:12 +0000

    files changes

--- login/index.php
+++ login/index.php
@@ -2,20 +2,20 @@
 <html>
 <head>
 	<title>Animated Login Form</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<img class="wave" src="img/wave.png">
+	<img class="wave" src="assets/img/wave.png">
 	<div class="container">
 		<div class="img">
-			<img src="img/bg.png">
+			<img src="assets/img/bg.png">
 		</div>
 		<div class="login-content">
 			<form action="index.html">
-				<img src="img/avatar.svg">
+				<img src="assets/img/avatar.svg">
 				<h2 class="title">Welcome</h2>
            		<div class="input-div one">
            		   <div class="i">
@@ -39,6 +39,6 @@
             </form>
         </div>
     </div>
-    <script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
 </body>
 </html>
